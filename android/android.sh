e0c621cf4f24aec10b6acfd6dd2c54e790c82072
Dekue <denis.kuechemann@web.de>
2016-03-25 22:54:31 +0100

    minor changes in android.sh

--- android/android.sh
+++ android/android.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
 
-# check if git is installed
+#################################################
+# preferences / debug                           #
+#################################################
+# Remove previously installed files and folders?
+RM_FILES=false
+
+# Save a lot of time by disabling this if already installed correctly.
+UNZIP_NDK=false
+
+
+#################################################
+# dependencies                                  #
+#################################################
+# Check if git is installed:
 type -P git &>/dev/null || { echo "You need to have \"git\" installed, but it is not.  Aborting."; exit 0; }
 
-# important configuration variables, check these if something went wrong
+# Important configuration variables, check these if something went wrong.
 GIT_LIBDESSERT=https://github.com/Dekue/libdessert
 
-# the android platform (e.g. android-3, android-4, android-5 ...)
+# Android API level (eg. android-21 = Android 5.0)
 ANDROID_PLATFORM=android-21
 
-# android-ndk
+# Android-NDK
 NDK_LOCATION=http://dl.google.com/android/repository
 NDK_FILE=android-ndk-r11b-linux-x86_64.zip
 
 # libpcap
-LIBPCAP_LOCATION=http://www.tcpdump.org/release/
-LIBPCAP_FILE=libpcap-1.1.1.tar.gz
+LIBPCAP_LOCATION=https://github.com/the-tcpdump-group/libpcap/archive
+LIBPCAP_FILE=libpcap-1.7.4.tar.gz
 
 # uthash
-UTHASH_LOCATION=http://downloads.sourceforge.net/uthash/
-UTHASH_FILE=uthash-1.9.7.tar.bz2
+UTHASH_LOCATION=https://github.com/troydhanson/uthash/archive
+UTHASH_FILE=v1.9.9.tar.gz
 
 # libcli
 GIT_LIBCLI=https://github.com/dparrish/libcli
 
 
-
-#############################################
-# more important variables...DO NOT CHANGE  #
-#############################################
+#################################################
+# Path variables depending on archive names and #
+# directory names in archive. Check these on an # 
+# update of uthash etc.                         #
+#################################################
 INSTALL_DIR=$1
 NDK_DIR=${NDK_FILE%"-linux-x86_64.zip"}
 LIBPCAP_DIR=${LIBPCAP_FILE%".tar.gz"}
-UTHASH_DIR=${UTHASH_FILE%".tar.bz2"}
+UTHASH_DIR=${UTHASH_FILE%".tar.gz"}
+UTHASH_DIR=${UTHASH_DIR#v}
+UTHASH_DIR=uthash-$UTHASH_DIR
 
 if [[ $INSTALL_DIR != --install_dir\=* ]]
 then
@@ -79,18 +95,21 @@ fi
 cd $INSTALL_DIR
 
 # cleanup old folders, keep downloaded files
-echo "Cleaning up old files (from previous installations)..."
-######rm -rf bin libdessert libcli android-ndk-*[^zip] uthash-*[^bz2] android-toolchain $LIBPCAP_FILE
-rm -rf libcli ######
+if $RM_FILES
+then
+	echo "Cleaning up old files (from previous installations)..."
+	rm -rf bin libdessert libcli android-ndk-*[^zip] uthash-*[^gz] android-toolchain
+fi
+rm -rf libcli libpcap-*[^gz]
 
-# Create necessary subdirectories
-echo "Creating bin subdirectory"
+# create bin subdirectory
+echo "Creating bin subdirectory..."
 mkdir bin
 
 # fetch ndk from configured location
 if [ ! -e "$NDK_FILE" ]
 then
-	echo "Downloading NDK... (794MB)" #405mb for 9d
+	echo "Downloading NDK... (794MB)"
 	wget -nc -q $NDK_LOCATION$NDK_FILE
 	if [ ! -e "$NDK_FILE" ]
 	then
@@ -104,21 +123,24 @@ echo "Checking out current libdessert from repository..."
 git clone -q $GIT_LIBDESSERT
 
 # install android-ndk and toolchain
-echo "Installing Android NDK..."
-#####unzip $NDK_FILE &> /dev/null #delete to save a lot of time if already installed correctly
+if $UNZIP_NDK
+then
+	echo "Installing Android NDK..."
+	unzip $NDK_FILE &> /dev/null
+fi
 cd $NDK_DIR"/build/tools"
 export ANDROID_NDK_ROOT=$INSTALL_DIR"/"$NDK_DIR
 export ANDROID_NDK_HOME=$INSTALL_DIR"/"$NDK_DIR
 ./make-standalone-toolchain.sh --ndk-dir=$INSTALL_DIR"/"$NDK_DIR --install-dir=$ANDROID_TOOLCHAIN
-echo "Setting ANDROID_TOOLCHAIN..."
 export ANDROID_TOOLCHAIN=$ANDROID_TOOLCHAIN
-echo "Android Toolchain dir set to $ANDROID_TOOLCHAIN"
+echo "Android Toolchain dir set to $ANDROID_TOOLCHAIN."
 if [ ! -d "$ANDROID_TOOLCHAIN" ]
 then
-	echo "Failed to install android toolchain. Aborting"
+	echo "Failed to install android toolchain. Aborting!"
 	exit 0
 fi
 cd $INSTALL_DIR
+
 
 # copy android-gcc and android-strip to bin directory
 echo "Copying android-gcc wrapper to bin..."
@@ -140,7 +162,7 @@ then
 	fi
 fi
 echo "Installing UTHASH headers..."
-tar xvjf $UTHASH_FILE &> /dev/null
+tar xvzf $UTHASH_FILE &> /dev/null
 cd $UTHASH_DIR"/src"
 cp *.h $INSTALL_DIR"/libdessert/include"
 cd $INSTALL_DIR
@@ -171,7 +193,7 @@ then
 fi
 
 # installing libcli
-echo "Installing libcli..."
+echo "Downloading and installing libcli..."
 git clone $GIT_LIBCLI
 if [ ! -d "libcli" ]
 then
@@ -194,7 +216,6 @@ fi
 
 # installing libpcap
 echo "Downloading libpcap..."
-
 if [ ! -e "$LIBCAP_FILE" ]
 then
 	wget -nc -q $LIBPCAP_LOCATION$LIBPCAP_FILE
@@ -206,8 +227,10 @@ then
 fi
 echo "Installing libpcap..."
 tar xvzf $LIBPCAP_FILE &> /dev/null
+mv libpcap-$LIBPCAP_DIR $LIBPCAP_DIR
 cd $LIBPCAP_DIR
-./configure CFLAGS="-Dlinux" --prefix=$INSTALL_DIR"/libdessert" --host=arm-none-linux --with-pcap=linux ac_cv_linux_vers=2 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
+
+./configure CFLAGS="-Dlinux" --prefix=$INSTALL_DIR"/libdessert" --host=arm-none-linux-gnueabi --with-pcap=linux ac_cv_linux_vers=2 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
 make &> build.log
 make install
 cd $INSTALL_DIR
@@ -216,15 +239,26 @@ then
 	echo "Failed to build libpcap. See \"libpcap/build.log\""
 	exit 0
 fi
-exit 0 #####
+exit 0 ##############################
 # building libdessert
 echo "Building libdessert..."
 cd libdessert
 sh autogen.sh
-./configure CFLAGS="-I$INSTALL_DIR/libdessert/include -I$ANDROID_NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm/usr/include -D__linux__" LDFLAGS="-L$INSTALL_DIR/libdessert/lib -L$ANDROID_NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm/usr/lib" --prefix=$INSTALL_DIR"/libdessert/" --host=arm-none-linux --without-net-snmp --enable-android-build ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
+CC="gcc-4.9" #change standard compiler back to gcc
+
+
+#--enable-android-build: checks pthreadex library - doesn't work right now
+./configure CFLAGS="-I$INSTALL_DIR/libdessert/include" LDFLAGS="-L$INSTALL_DIR/libdessert/lib" --host=arm-none-linux --without-net-snmp ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
+
+##### old FLAGS #####
+#CFLAGS="-I$INSTALL_DIR/libdessert/include -I$ANDROID_NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm/usr/include -D__linux__"
+#LDFLAGS="-L$INSTALL_DIR/libdessert/lib -L$ANDROID_NDK_HOME/platforms/$ANDROID_PLATFORM/arch-arm/usr/lib"
+#--prefix=$INSTALL_DIR"/libdessert/" --host=arm-none-linux --without-net-snmp --enable-android-build ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
+#####################
 # setting the CPPFLAGS fixes a flaw in the configure script, where always the standard include "/usr/include" is appended to the compiler flags
 make CPPFLAGS="" &> build.log
 make install
+
 cd $INSTALL_DIR
 if [ ! -e "libdessert/lib/libdessert.a" ]
 then
@@ -234,10 +268,10 @@ fi
 
 # cleanup
 echo "Cleaning up..."
-rm *.tar.gz
-rm *.bz2
-rm -rf libcli libcli-patch libdessert libpcap-1.1.1 libpthreadex libregex uthash-1.9.3
-
+#rm *.tar.gz
+#rm -rf libcli libcli-patch libdessert libpcap-*[^gz] libpthreadex libregex uthash-*[^gz]
+echo "MARK"
+exit 0 #####
 # Building archive
 echo "Building archive..."
 tar cvzf libdessert_android.tar.gz libdessert &> /dev/null
